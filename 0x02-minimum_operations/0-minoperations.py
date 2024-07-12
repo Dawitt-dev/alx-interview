dce2a373047920c3707462d419139627d86ee62d
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-12 00:05:23 +0000

    interview 3

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,8 @@
 write a method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
 """
+
+
 def minOperations(n):
     if n <= 1:
         return 0
