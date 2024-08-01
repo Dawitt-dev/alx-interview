0853acb0092b675c898e6c89880c227946885472
“Dawitt-dev” <davoza27@yahoo.com>
2024-08-01 14:17:16 -0700

    task_0. N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_usage_and_exit():
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+
+def validate_input(args):
+    if len(args) != 2:
+        print_usage_and_exit()
+    try:
+        N = int(args[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return N
+
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens(board, col):
+    if col >= len(board):
+        print_solution(board)
+        return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def main():
+    N = validate_input(sys.argv)
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens(board, 0)
+
+
+if __name__ == "__main__":
+    main()
