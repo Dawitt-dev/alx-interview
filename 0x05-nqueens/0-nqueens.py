4ac65eaf5d0e7bed774089b5b9945a640e21731a
“Dawitt-dev” <davoza27@yahoo.com>
2024-08-01 14:35:58 -0700

    task_0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,65 +1,83 @@
 #!/usr/bin/python3
+"""N queens solution finder module.
+"""
 import sys
 
 
-def print_usage_and_exit():
-    print("Usage: nqueens N")
-    sys.exit(1)
+def get_input():
+    """Retrieves and validates this program's argument.
 
-
-def validate_input(args):
-    if len(args) != 2:
-        print_usage_and_exit()
+    Returns:
+        int: The size of the chessboard.
+    """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
     try:
-        N = int(args[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return N
+    return n
 
 
-def is_safe(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
+def is_attacking(pos0, pos1):
+    """Checks if the positions of two queens are in an attacking mode.
 
+    Args:
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
 
-def solve_nqueens(board, col):
-    if col >= len(board):
-        print_solution(board)
+    Returns:
+        bool: True if the queens are in an attacking position else False.
+    """
+    if pos0[0] == pos1[0] or pos0[1] == pos1[1]:
         return True
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_nqueens(board, col + 1) or res
-            board[i][col] = 0
-    return res
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
 
 
-def print_solution(board):
-    solution = []
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 1:
-                solution.append([i, j])
-    print(solution)
+def build_solution(n, row, positions, solutions):
+    """Builds a solution for the n queens problem.
+
+    Args:
+        n (int): The size of the chessboard.
+        row (int): The current row in the chessboard.
+        positions (list of lists of integers): The positions of placed queens.
+        solutions (list of lists): The list of possible solutions.
+    """
+    if row == n:
+        solutions.append(positions.copy())
+        return
+    for col in range(n):
+        valid = True
+        for pos in positions:
+            if is_attacking(pos, [row, col]):
+                valid = False
+                break
+        if valid:
+            positions.append([row, col])
+            build_solution(n, row + 1, positions, solutions)
+            positions.pop()
+
+
+def print_solutions(solutions):
+    """Prints the solutions in the required format.
+
+    Args:
+        solutions (list of lists): The list of possible solutions.
+    """
+    for solution in solutions:
+        print(solution)
 
 
 def main():
-    N = validate_input(sys.argv)
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_nqueens(board, 0)
+    n = get_input()
+    solutions = []
+    build_solution(n, 0, [], solutions)
+    print_solutions(solutions)
 
 
 if __name__ == "__main__":
