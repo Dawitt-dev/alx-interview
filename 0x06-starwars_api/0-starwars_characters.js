5007e5a5da67e8ac7cd03565960055eb69d7ae60
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-08-08 22:14:13 +0000

    trial

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,26 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const characters = JSON.parse(body).characters;
-  characters.forEach(character => {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
       console.log(JSON.parse(body).name);
-    });
+      req(arr, i + 1);
+    }
   });
-});
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
