53c1e0249d2b2df48fd9285ce47962851975abf7
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-08-29 10:21:39 +0000

    pycodestyled

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" a function that returns the perimeter of 
+""" a function that returns the perimeter of
     the island described in grid
 """
+
+
 def island_perimeter(grid):
     """calculates the perimeter of an island
         represented in a 2D grid
@@ -9,7 +11,7 @@ def island_perimeter(grid):
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
@@ -25,5 +27,5 @@ def island_perimeter(grid):
                 # Check right neighbor
                 if j == cols-1 or grid[i][j+1] == 0:
                     perimeter += 1
-    
+
     return perimeter
